1fd7fffa4511391af1a1cd5eb56b02c3f90ae3c1
ashwani <ashwanichandel94964@gmail.comm>
2023-05-26 12:10:26 +0530

    first

--- flutter_application_2/lib/screens/search.dart
+++ flutter_application_2/lib/screens/search.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_application_2/screens/tabtabr.dart';
+import 'package:flutter_application_2/screens/tabbar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../main.dart';
